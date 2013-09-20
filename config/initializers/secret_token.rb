97580b5330bdb85ffeebfde42c78ed0a3027d192
rohan1020 <rohanraja9@gmail.com>
2013-09-21 00:59:55 +0530

    innitial BS WORKING

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = 'b9be3dcc82ae0f7dee654b73f08398e8371e063e55d6c115a14dc911363d9e2fbdc1978d9858565e56eb8e35adb1040fd64319557a49cc033fe1c102cf3cb261'
